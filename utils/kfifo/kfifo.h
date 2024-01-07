43cead3842fb152add319835130856a19515f5c3
Jacob Abel <jacobabel@nullpo.dev>
2024-01-07 03:56:40 -0500

    kfifo.h: prune vestigial code
    Signed-off-by: Jacob Abel <jacobabel@nullpo.dev>

--- utils/kfifo/kfifo.h
+++ utils/kfifo/kfifo.h
@@ -34,7 +34,6 @@ unsigned int kfifo_get(struct kfifo *k, unsigned char *buff, unsigned int len);
 unsigned int kfifo_peek(struct kfifo *k, unsigned int len_want, unsigned char **buff1, unsigned char **buff2, unsigned int *len1, unsigned int *len2);
 unsigned int kfifo_peek_to_buf(struct kfifo *fifo, unsigned char *buff, unsigned int len);
 unsigned int kfifo_len(struct kfifo *fifo);
-unsigned int kfifo_available(struct kfifo *fifo);
 
 #if defined(__cplusplus)
 }
