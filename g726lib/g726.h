7f1fc8e20e14819d52367965804242272323e2d3
Jacob Abel <jacobabel@nullpo.dev>
2024-01-10 06:21:42 -0500

    foscam-ios-sdk/282cc7f68d: add extern c tag to g726.h
    Signed-off-by: Jacob Abel <jacobabel@nullpo.dev>

--- g726lib/g726.h
+++ g726lib/g726.h
@@ -1,7 +1,15 @@
 #ifndef __G726_H
 #define __G726_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void g726_Encode(unsigned char *speech,char *bitstream);
 extern void g726_Decode(char *bitstream,unsigned char *speech);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
