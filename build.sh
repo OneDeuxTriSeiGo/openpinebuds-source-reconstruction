238404e90ca215066f6683ec84616095538fb06f
Jacob Abel <jacobabel@nullpo.dev>
2024-02-23 01:49:30 -0500

    Delete files unrelated to diff
    Signed-off-by: Jacob Abel <jacobabel@nullpo.dev>

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make -j T=open_source DEBUG=1 >log.txt 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "build success"
-else
-    echo "build failed and call log.txt"
-    cat log.txt | grep "error:*"
-fi
--- download.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-num=$(ls -l /dev/ttyUSB* | rev | cut -c 1)
-#num=3
-echo com is:$num
-sudo -S dldtool -c $num -f out/open_source/open_source.bin
-sudo minicom port$num
